cc51a516bbfd5743533afab474a4e4c5bde5a5e5
invigorating92 <84106746+invigorating92@users.noreply.github.com>
2021-08-06 01:08:34 +0900

    add#6.3

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import express from "express";
-import {globalRouter} from "./routers/globalRouter";
-import {userRouter} from "./routers/userRouter";
-import {videoRouter} from "./routers/videoRouter";
+import { globalRouter } from "./routers/globalRouter";
+import { userRouter } from "./routers/userRouter";
+import { videoRouter } from "./routers/videoRouter";
 
 console.log("how are you Thank you");
 
@@ -9,15 +9,17 @@ const PORT = 4000;
 
 const app = express();
 
-const handleListening =() => console.log(`안녕디지몬 http://localhost:${PORT}`)
+const handleListening = () =>
+  console.log(`안녕디지몬 http://localhost:${PORT}`);
 
 // const handleHome=()=> console.log("somebody is trying to go home.")
 
 app.set("view engine", "pug");
-app.set("views", process.cwd()+"/src/views");
+app.set("views", process.cwd() + "/src/views");
+app.use(express.urlencoded({ extended: true }));
 app.use("/", globalRouter);
 app.use("/user", userRouter);
 app.use("/video", videoRouter);
 // app.get("/", handleHome);
 
-app.listen(PORT, handleListening)
+app.listen(PORT, handleListening);
